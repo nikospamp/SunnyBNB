3483fe3c66aeb527fadaa308a7a6d7eb2501c48a
Nikos <pampoukidis@arx.gr>
2017-05-29 16:28:35 +0300

    Init

---
+++ app/src/main/java/com/example/arxdev30/sunnybnb/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.example.arxdev30.sunnybnb;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Pampoukidis on 29/5/2017.
+ */
+
+public class HomeFragment extends android.support.v4.app.Fragment {
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Home");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home, container, false);
+    }
+}
