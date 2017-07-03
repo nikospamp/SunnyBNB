ff87be00deb1b717433fb835cfaf85cad6483c5b
Nikos <pampoukidis@arx.gr>
2017-07-03 18:17:49 +0300

    dummy change

--- app/src/main/java/com/example/ptuxiaki/sunnybnb/MainActivity.java
+++ app/src/main/java/com/example/ptuxiaki/sunnybnb/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        Log.d("Here", "1");
+        Log.d("Here", "2");
         displaySelectedScreen(R.id.nav_home);
     }
 
