8af0977186587b96d98ff013b301efdb05a6e6fe
zigoura <mariazigoura@gmail.com>
2017-07-12 21:38:44 +0300

    i changed the background colour and the textview at the nav_header_main.xml i also changed all the profilefragment.java class (ama kaneis sindesi me gmail travaei sto profil aytomata apo to dataprovider tis google eikona ,meil kai onoma),i added one more dependancy at the app (volley:volley)

--- app/src/main/java/com/example/ptuxiaki/sunnybnb/HomeFragment.java
+++ app/src/main/java/com/example/ptuxiaki/sunnybnb/HomeFragment.java
@@ -32,9 +32,9 @@ public class HomeFragment extends android.support.v4.app.Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.profile, container, false);
+        View view = inflater.inflate(R.layout.home, container, false);
 
-        mButton = (Button) view.findViewById(R.id.loginBtn);
+//        mButton = (Button) view.findViewById(R.id.loginBtn);
         return view;
     }
 }
