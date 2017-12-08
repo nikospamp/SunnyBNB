030e5f60f6990f72e9ea73d4c3b50c3b1e50ccf6
Nikos <pampoukidis@arx.gr>
2017-12-08 21:15:08 +0200

    Top destinations are being displayed correctly and gets real-time changes

---
+++ app/src/main/java/com/example/ptuxiaki/sunnybnb/ui/TopDestinations/TopDestinationsAdapter.kt
@@ -0,0 +1,56 @@
+package com.example.ptuxiaki.sunnybnb.ui.TopDestinations
+
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.ptuxiaki.sunnybnb.R
+import com.google.firebase.database.Query
+import kotlinx.android.synthetic.main.top_destination_single.view.*
+
+/**
+ * Created by arxdev30 on 8/12/2017.
+ */
+
+class TopDestinationsAdapter(var cities: ArrayList<String>?, var citiesCounter: ArrayList<Int>?, var listener: ((query: Query) -> Unit)?)
+    : RecyclerView.Adapter<TopDestinationsAdapter.CustomViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.top_destination_single, parent, false)
+
+        for ((index, value) in citiesCounter?.withIndex()!!) {
+            Log.d("TopDestinationsAdapter", "$index: $value")
+        }
+
+        val customViewHolder = CustomViewHolder(itemView)
+
+        itemView.setOnClickListener({
+
+        })
+
+        return customViewHolder
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder?, position: Int) {
+        val cityName = cities?.get(position)
+        val cityCounter = citiesCounter?.get(position)
+        holder?.bindTo(cityName, cityCounter)
+    }
+
+    override fun getItemCount(): Int = cities?.count() ?: 0
+
+    class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val cityNameTv: TextView = view.top_destination_single_city_name
+        private val cityCounterTv = view.top_destination_single_counter
+
+        fun bindTo(cityName: String?, cityCounter: Int?) {
+            cityNameTv.text = cityName
+            cityCounterTv.text = cityCounter.toString()
+        }
+
+    }
+
+}
